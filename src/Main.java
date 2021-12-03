8519c94630157cf1a307c68738fb4878c6a27153
STANISLAV LITVINOV <91020862+Stanislav-Litvinov@users.noreply.github.com>
2021-12-03 03:36:05 +0300

    Added Bmi.java files

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        BmiService bodyMassIndex = new BmiService();
+
+        double bmiLow = bodyMassIndex.calculate(200, 43);
+        System.out.println("ИМТ равен: " + bmiLow);
+
+        double bmiMinimal = bodyMassIndex.calculate(170, 57);
+        System.out.println("ИМТ равен: " + bmiMinimal);
+
+        double bmiNormal = bodyMassIndex.calculate(174, 75);
+        System.out.println("ИМТ равен: " + bmiNormal);
+
+        double bmiOverWeight = bodyMassIndex.calculate(176, 82);
+        System.out.println("ИМТ равен: " + bmiOverWeight);
+
+        double bmiObesity1 = bodyMassIndex.calculate(175, 100);
+        System.out.println(bmiObesity1);
+
+        double bmiObesity2 = bodyMassIndex.calculate(175, 120);
+        System.out.println("ИМТ равен: " + bmiObesity2);
+
+        double bmiObesity3 = bodyMassIndex.calculate(175, 200);
+        System.out.println("ИМТ равен: " + bmiObesity3);
+
+    }
+}
